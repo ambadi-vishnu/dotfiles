7dcb2552bacad5f5f7ed8a797c1d96e1d99736f3
Ambadi Vishnu <70402710+ambadi-vishnu@users.noreply.github.com>
2025-04-18 23:45:08 +0000

    Minor Tweaks

--- custom/scripts/mirror.sh
+++ custom/scripts/mirror.sh
@@ -8,5 +8,4 @@ echo "$(date '+%A %d %B %Y | %I:%M %p'): Success: Rate-Mirrors Installed" >> ~/i
 #Update Pacman Mirrorlist
 echo "$(date '+%A %d %B %Y | %I:%M %p'): Action: Updating Pacman Mirrorlist..." >> ~/install-log.txt
 rate-mirrors --protocol https arch | sudo tee /etc/pacman.d/mirrorlist || { echo "$(date '+%A %d %B %Y | %I:%M %p'): Error: Failed Updating Mirrorlist" >> ~/install-log.txt; exit 1; }
-sudo pacman -Syy || { echo "$(date '+%A %d %B %Y | %I:%M %p'): Error: Failed Syncing Package Database" >> ~/install-log.txt; exit 1; }
 echo "$(date '+%A %d %B %Y | %I:%M %p'): Success: Pacman Mirrorlist Updated" >> ~/install-log.txt
